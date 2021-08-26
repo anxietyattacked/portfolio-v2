35f33a51d6d572c4c2c6a89ecddcf80ed945097c
Michael Bergerson <michealbergerson@gmail.com>
2021-08-26 13:36:39 -0700

    spotify-clone update

--- data/projectInfo.js
+++ data/projectInfo.js
@@ -1,37 +1,43 @@
 export const projectData = [
-    {
-        name: "SneakerFlex",
-        image:"/images/SneakerFlex.png",
-        info:"SneakerFlex is a full stack sneaker ecommerce website built in Nextjs and Node using TypeScript for both the backend and frontend The backend uses postrgesql for a database, graphql and typeorm for the API. Users can register, login, create, update and delete comments and create orders using Stripe as a payment handler. ",
-        tech:"Typescript, Nextjs, Urql, Tailwind, Formik",
-        techB:"Typescript, Node, Express, Graphql, Postgresql, Typeorm, Redis",
-        ghLink:"https://github.com/anxietyattacked/shoe-ecomV2",
-        Link:"https://sneakerflex.vercel.app/",
-        iWidth: 1920,
-        iHeight:880
-
-        
-    },
-    {
-        name:"HoopyDo",
-        image:"/images/HoopyDo.png",
-        info:"HoopyDo is an NBA statistics and shot chart explorer built in React, Tailwind, and D3. The data is from nba_api and I used Pythons’ Pandas library to manipulate the data. Users can select a player and see their season totals, shot charts and stats by game.",
-        tech:"React, D3, Tailwind, React Table, data from nba_api, Python, Pandas",
-        ghLink:"https://github.com/anxiety,attacked/NBA-Stats-Dashboard",
-        Link:"https://naughty-neumann-fb4b06.netlify.app/",
-        iWidth: 1920,
-        iHeight:927
-    },
-    {
-        name:"Markalytics",
-        image:"/images/Markalytics.png",
-        info:"Markalytics is a website and blog for a marketing and analytics company built in React. It features a video background for desktops, hamburger menu for mobile, a working contact form, and a paginated blog with articles.",
-        tech:"React",
-        ghLink:"https://github.com/anxietyattacked/Markalytics",
-        Link: "https://dazzling-chandrasekhar-be3f8e.netlify.app/",
-        iWidth: 1909,
-        iHeight: 927
-        
-
-    }
-]
+  {
+    name: "Spotify Clone with Music Visualizer",
+    image: "/images/spotify.jpg",
+    info: "I built a Spotify clone with in with a music visualizer in Nextjs, Typescript and Three.js. It features a fully fuctioning audio player in which you can play, pause, skip, seek, and adjust volume. The music visualizer was built in Three.js and will rotate, pusle and change color to the average frequency of the song.",
+    tech: "TypeScript, Nextjs, Three.js",
+    ghLink: "https://github.com/anxietyattacked/spotify-clone",
+    Link: "https://spotify-clone-beryl.vercel.app/",
+    iWidth: 1920,
+    iHeight: 902,
+  },
+  {
+    name: "SneakerFlex",
+    image: "/images/SneakerFlex.png",
+    info: "SneakerFlex is a full stack sneaker ecommerce website built in Nextjs and Node using TypeScript for both the backend and frontend The backend uses postrgesql for a database, graphql and typeorm for the API. Users can register, login, create, update and delete comments and create orders using Stripe as a payment handler. ",
+    tech: "TypeScript, Nextjs, Urql, Tailwind, Formik",
+    techB: "TypeScript, Node, Express, Graphql, Postgresql, Typeorm, Redis",
+    ghLink: "https://github.com/anxietyattacked/shoe-ecomV2",
+    Link: "https://sneakerflex.vercel.app/",
+    iWidth: 1920,
+    iHeight: 880,
+  },
+  {
+    name: "HoopyDo",
+    image: "/images/HoopyDo.png",
+    info: "HoopyDo is an NBA statistics and shot chart explorer built in React, Tailwind, and D3. The data is from nba_api and I used Pythons’ Pandas library to manipulate the data. Users can select a player and see their season totals, shot charts and stats by game.",
+    tech: "React, D3, Tailwind, React Table, data from nba_api, Python, Pandas",
+    ghLink: "https://github.com/anxiety,attacked/NBA-Stats-Dashboard",
+    Link: "https://naughty-neumann-fb4b06.netlify.app/",
+    iWidth: 1920,
+    iHeight: 927,
+  },
+  {
+    name: "Markalytics",
+    image: "/images/Markalytics.png",
+    info: "Markalytics is a website and blog for a marketing and analytics company built in React. It features a video background for desktops, hamburger menu for mobile, a working contact form, and a paginated blog with articles.",
+    tech: "React",
+    ghLink: "https://github.com/anxietyattacked/Markalytics",
+    Link: "https://dazzling-chandrasekhar-be3f8e.netlify.app/",
+    iWidth: 1909,
+    iHeight: 927,
+  },
+];
