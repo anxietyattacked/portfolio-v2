464e6feab4cd35b81b4cb5139973b072d158e1b5
Michael Bergerson <michealbergerson@gmail.com>
2022-01-11 21:28:09 -0800

    update

--- data/dataProject.js
+++ data/dataProject.js
@@ -1,8 +1,16 @@
 export const dataProject = [
   {
+    name: "Basketball Player Analysis in SQL and Python",
+    image: "/images/Basketball Player Analysis in SQL and Python.png",
+    info: "I used a database which contains NBA player and game information from the start of the NBA to the 2019-2020 season. I Used Python and SQL to query a SQLite database and analyze the data. I then created data visualizations using Plotly",
+    Link: "https://www.kaggle.com/michaelbergerson/us-car-accidents-traffic-data-analysis",
+    iWidth: 1920,
+    iHeight: 892,
+  },
+  {
     name: "US Car Accidents Traffic Dataset Analysis In Python",
     image: "/images/US Car Accidents Traffic Data Analysis.png",
-    info: "Dataset contains 1.5 million records of US car accidents that impacted traffic. I Used Python and Pandas to manipulate and clean the data. I Created data visualizations using Plotly",
+    info: "Dataset contains 1.5 million records of US car accidents that impacted traffic. I Used Python and Pandas to manipulate and clean the data. I then Created data visualizations using Plotly",
     Link: "https://www.kaggle.com/michaelbergerson/us-car-accidents-traffic-data-analysis",
     iWidth: 1920,
     iHeight: 892,
