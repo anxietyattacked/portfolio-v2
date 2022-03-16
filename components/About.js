44eb64a0b15f2e49b51239cdabd6f1762cf2d9d6
Michael Bergerson <michealbergerson@gmail.com>
2022-03-15 22:29:55 -0700

    refresh v0.001

--- components/About.js
+++ components/About.js
@@ -11,12 +11,12 @@ export const About = () => {
         I graduated from Oregon State University with a Bachelors of Science in
         Economics. After college I started learning Python for data analysis.
         After learning the basics of programing, I wanted to learn the data
-        visualization libarary d3.js and web development in general. So I
-        learned JavaScript, React, CSS, HTML and started building projects. I
-        have also continued learning about data analysis and data science as
-        well. For data analysis I frequently use Python, R, SQL, Tableau, Excel,
-        Pandas, Tidyverse, and Plotly. For web development I frequently
-        JavaScript TypeScript, React, CSS, HTML, Node, GraphQL, and D3.js.
+        visualization library d3.js and web development in general. So I learned
+        JavaScript, React, CSS, HTML and started building projects. I have also
+        continued learning about data analysis and data science as well. For
+        data analysis I frequently use Python, R, SQL, Tableau, Excel, Pandas,
+        Tidyverse, and Plotly. For web development I frequently JavaScript
+        TypeScript, React, CSS, HTML, Node, GraphQL, and D3.js.
       </p>
     </section>
   );
